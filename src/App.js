f19c017e97c6131cef87223e49e335fe322b0c96
dzehil02 <dzehil02@gmail.com>
2024-06-14 17:11:30 +0200

    feat: add header with burger-menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import Main from './components/layout/Main';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='title'>Hello SpaceX</h1>
+      <Header/>
+      <Main />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/layout/Main.jsx
@@ -0,0 +1,17 @@
+function Main() {
+  return (
+      <main className='main'>
+        <h1 className='title'>Hello SpaceX</h1>
+        <p className='subtitle'>Discover the vast expanses of space</p>
+        <div className='main__button'>
+          <a href='/' className='button button_yellow'>Learn more</a>
+        </div>
+        <div className='main__button'>
+        <a href='/' className='button button_outline'>Learn more</a>
+        </div>
+      </main>
+
+  );
+}
+
+export default Main;
